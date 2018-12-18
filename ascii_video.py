c54321405df3d86954b57fe1907df15228a0970f
Ali Shazly <alishazly302@gmail.com>
2018-12-17 21:04:01 -0800

    added error checking and made argparse work

--- ascii_video.py
+++ ascii_video.py
@@ -1,21 +1,20 @@
 import numpy as np
 import cv2
 from PIL import Image
-import time 
+import time
 import sys
 import os
 import pafy
 import argparse
 
-chars = ['@', '#', 'S', '%', '?', '*', '+', ';', ':', ',', '.']
-chars = chars[::-1]
+chars = ['.', ',', ':', ';', '+', '*', '?', '%', 'S', '#', '@']
 
 def cv_to_pillow(image):
     img_RGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     pil_img = Image.fromarray(img_RGB)
     return pil_img
 
-def image_resize(image, width = None, height = None):
+def image_resize(image, args, width=None, height=None):
     dim = None
     (old_width, old_height) = image.size
     if width is None and height is None:
@@ -29,59 +28,81 @@ def image_resize(image, width = None, height = None):
     resized = image.resize(dim)
     return resized, dim
 
-def image_to_ascii_grayscale(image):
+def image_to_ascii_grayscale(image, args):
+    resolution = args.resolution
     image = image.convert('L')
     grayscale_values = list(image.getdata())
     ascii_pixels = ''.join(chars[i//25] for i in grayscale_values)
-    ascii_image = [ascii_pixels[i:i+resolution] for i in range(0, len(ascii_pixels), resolution)]
+    ascii_image = [ascii_pixels[i:i+resolution]
+                   for i in range(0, len(ascii_pixels), resolution)]
     return ascii_image
 
-def get_frame_data(): # TODO add a progress bar
+def get_video_data(args):
     frame_list = []
-    print('Converting to ASCII, do not resize window.')
+    resolution = args.resolution
+    if args.youtube:
+        try:
+            url = args.youtube[-11:]
+            vPafy = pafy.new(url)
+            play = vPafy.getbest(preftype="webm")
+            cap = cv2.VideoCapture(play.url)
+        except AttributeError as e:
+            sys.stdout.write(f'ERROR: Youtube music links do not currently work')
+            sys.exit()
+    elif args.file:
+        try:
+            cap = cv2.VideoCapture(args.file)
+            if cap.read() == (False, None):
+                raise FileNotFoundError(f'File path {args.file} not found.')
+        except FileNotFoundError as e:
+            sys.stdout.write(f'ERROR: {e}')
+            sys.exit()
+
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    iteration = 0
     while True:
+        iteration +=1
+        print(f'Converting to ASCII, do not resize window... {int(iteration/length*100)}%', end = '\r')
         ret, frame = cap.read()
         if ret:
             pil_frame = cv_to_pillow(frame)
-            resized, dim = image_resize(pil_frame, width = resolution)
-            frame_list.append(('\n'.join(image_to_ascii_grayscale(resized))))
+            resized, dim = image_resize(pil_frame, args, width=resolution)
+            ascii_image = image_to_ascii_grayscale(resized, args)
+            frame_list.append('\n'.join(ascii_image))
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 cap.release()
                 cv2.destroyAllWindows()
                 return
         else:
+            os.system('cls' if os.name == 'nt' else 'clear')
             os.system(f'mode con: cols={dim[0]} lines={dim[1]}')
             cap.release()
             cv2.destroyAllWindows()
-            return frame_list
+            return frame_list, fps
 
-def display_terminal():
-    frame_list = get_frame_data()
+def display_terminal(args):
+    frame_list, fps = get_video_data(args)
     while True:
         for i in frame_list:
-            print(i)
+            sys.stdout.write(i)
             time.sleep(1/fps)
 
-# TODO: put this in a main() func
-# TODO: merge this with webcam
-parser = argparse.ArgumentParser()
-mods = parser.add_mutually_exclusive_group()
-parser.add_argument('-r', '--resolution', type=int, default=100,
-                    help='Width to resize the image to, in pixels. Higher value means more detail. Default=100')
-mods.add_argument('-y','--youtube', type=str, default = None ,
-                    help='Use a youtube video as input')
-mods.add_argument('-f', '--file', type=str, default = None,
-                    help='File to convert to ascii')
-args = parser.parse_args()
+def main(): 
+    # TODO: Merge with webcam
+    # TODO: Add -r for realtime playing
+    parser = argparse.ArgumentParser()
+    mods = parser.add_mutually_exclusive_group()
+    parser.add_argument('-r', '--resolution', type=int, default=100,
+                        help='Width to resize the image to, in pixels. Higher value means more detail.')
+    mods.add_argument('-y', '--youtube', type=str, default=None,
+                      help='Use a youtube video as input')
+    mods.add_argument('-f', '--file', type=str, default=None,
+                      help='File to convert to ascii')
+    # mods.add_argument('-w', '--webcam', action='store_true',
+    #                   help='Use webcam as video input')
+    args = parser.parse_args()
+    display_terminal(args)
 
-resolution = args.resolution
-if args.youtube:
-    url = args.youtube
-    vPafy = pafy.new(url)
-    play = vPafy.getbest(preftype="webm")
-    cap = cv2.VideoCapture(play.url)
-elif args.file:
-    cap = cv2.VideoCapture(args.file)
-
-fps = cap.get(cv2.CAP_PROP_FPS)
-display_terminal()
+if __name__ == '__main__':
+    main()
